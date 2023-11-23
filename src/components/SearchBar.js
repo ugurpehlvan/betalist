cb5cac64e4d648d464d3bc1fc6b2c31b3cf058e1
ugurpehlvan <hireugurpehlivan@gmail.com>
2023-11-23 23:56:10 +0300

    Add header

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,28 +3,70 @@ import React from 'react';
 // components
 import { 
     AppBar, 
-    Toolbar,
-    Typography,
+    Card,
+    IconButton,
     InputBase,
+    Grid,
 } from '@mui/material';
 
-import SearchIcon from '@mui/icons-material/Search';
+// icons
+import { Cancel, Search} from '@mui/icons-material';
 
 const Header = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div">
-            beta
-        </Typography>
-        <div sx={{ display: 'flex', alignItems: 'center' }}>
-          <InputBase
-            placeholder="Search..."
-            inputProps={{ 'aria-label': 'search' }}
-          />
-          <SearchIcon sx={{ ml: 1 }} />
-        </div>
-      </Toolbar>
+      	<Grid sx={{
+            backgroundColor: 'white',
+            p: '1rem 0',
+        }}>
+            <Grid maxWidth={1360} container sx={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                margin: '0 auto',
+            }}>
+                <img style={{ width: "120px" }} src='./app-logo.png' alt='logo' className='logo' />
+                <Card
+					component="form"
+					sx={{ p: '0px 0px 0px 10px', display: 'flex', alignItems: 'center', width: 700, borderRadius: '20px' }}
+				>
+					<IconButton sx={{ p: '0 5px 0 5px' }} aria-label="menu">
+						<Search />
+					</IconButton>
+					<InputBase
+						sx={{ ml: 1, flex: 1 }}
+						placeholder="Searching for..."
+						// value={searchParam}
+						// onChange={(e) => dispatch(productActions.setSearchParam(e.target.value))}
+						// onKeyDown={keyPressHandler}
+					/>
+						{/* {searchParam && <IconButton onClick={() => {
+							dispatch(productActions.setSearchParam(''));
+						}} sx={{ p: '0 5px 0 5px' }} aria-label="close">
+							<Cancel />
+						</IconButton>} */}
+					<IconButton id='search-button'
+						type="button"
+						onClick={() => {
+							// dispatch(productActions.setIsSearched(true))
+							// search()
+						}}
+						sx={[
+							{
+								p: '12.5px 50px',
+								backgroundColor: '#C24B5A',
+								borderRadius: '0px 20px 20px 0px',
+
+							},
+							{
+								'&:hover': { backgroundColor: '#de3b50' }
+							}
+						]} aria-label="search">
+						<div style={{ fontSize: '16px', color: 'white' }}>Search</div>
+					</IconButton>
+				</Card>
+            </Grid>
+        </Grid>
     </AppBar>
   );
 };
